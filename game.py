82e8ad409f6a8c65999eccb65006c36aa065efd8
Garrett Lear <garrettlear@terpmail.umd.edu>
2025-05-05 17:34:31 -0400

    Create and shuffle the deck of cards

--- game.py
+++ game.py
@@ -1,7 +1,16 @@
 import random
 
-#List of card ranks (Ace, 2-10, Jack, Queen)
+#List of card ranks
 card_ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen']
 
 #List of suits
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+
+#Function to create a deck of cards
+def create_deck():
+    deck = []
+    for suit in suits:
+        for rank in card_ranks:
+            deck.append(rank + ' of ' + suit)
+    random.shuffle(deck) #Shuffle the deck to randomize the order
+    return deck
