4d4bc23e21c6448f50f4122fc6fc1327a75fc894
grlear@terpmail.umd.edu <garrettlear@Garretts-MacBook-Air.local>
2025-05-05 14:53:56 -0400

    First commit: Set up card rank and suits

---
+++ game.py
@@ -0,0 +1,7 @@
+import random
+
+#List of card ranks (Ace, 2-10, Jack, Queen)
+card_ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen']
+
+#List of suits
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
